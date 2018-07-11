9e29e35dce736d00be9d17086339c2334f33e91d
JohnWebb4 <John.Webb94@gmail.com>
2018-07-11 15:53:23 -0700

    Setup eslint and jest

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+      "no-bitwise": 0,
+      "no-extend-native": 0,
+      "no-param-reassign": 0,
+      "no-underscore-dangle": 0,
+      "prefer-rest-params": 0,
+      "prefer-template": 0,
+  },
+  "env": {
+      "browser": true,
+      "jquery": true,
+      "node": true,
+  }
+};
---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Change log
+
+## 0.1
+### New
+- @JohnWebb4 Create maintainers, changelog, git ignore, and packages json
+
+## Initial
+### New
+- @JohnWebb4 Create license and basic README
