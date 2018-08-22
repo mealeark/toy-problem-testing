3de0e37aa77e5c8ef925a313d3995407673e4e14
Mealear <mileyy.mk@gmail.com>
2018-08-22 15:48:09 -0700

    Add test coverage for reverseInteger

--- reverseInteger/reverseInteger.test.js
+++ reverseInteger/reverseInteger.test.js
@@ -1,7 +1,21 @@
 import reverseInteger from './reverseInteger';
 
 describe('reverseInteger', () => {
-    test('is a function', () => {
-      expect(reverseInteger).toBeInstanceOf(Function);
-    });
+  test('is a function', () => {
+    expect(reverseInteger).toBeInstanceOf(Function);
+  });
+
+  test('it returns the integer reversed', () => {
+    expect(reverseInteger(123)).toEqual(321);
+    expect(reverseInteger(-123)).toEqual(-321);
+    expect(reverseInteger(120)).toEqual(21);
+  });
+
+  test('it returns a number', () => {
+    expect(reverseInteger(1200)).toEqual(21);
+  });
+
+  test('it returns 0 for a result within the 32-bit signed integer range', () => {
+    expect(reverseInteger(2147483648)).toEqual(0);
+  });
 });
