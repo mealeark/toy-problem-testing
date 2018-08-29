425652a6d4a307bc8ee788a918377bee746009f4
Mealear <mileyy.mk@gmail.com>
2018-08-29 16:28:29 -0700

    Fix toy problem and prepare OICE

---
+++ minimumEditDistance/minimumDeleteSum.test.js
@@ -0,0 +1,11 @@
+import minimumEditDistance from './minimumEditDistance';
+
+describe('minimumEditDistance', () => {
+  test('is a function', () => {
+    expect(minimumEditDistance).toBeInstanceOf(Function);
+  });
+
+  // test('it returns true', () => {
+  //   expect(addTwoNumbers(5)).toBeTruthy();
+  // });
+});
