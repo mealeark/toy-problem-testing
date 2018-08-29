f93f7a83560f2b07a4ff9ebe84ae8ae0b62b5007
Mealear <mileyy.mk@gmail.com>
2018-08-29 15:27:05 -0700

    Add minimumDeleteSum and its tests

---
+++ minimumDeleteSum/minimumDeleteSum.js
@@ -0,0 +1,8 @@
+let minimumDeleteSum = function(s1, s2) {
+    
+};
+
+if (window.DEBUG) {
+  module.exports = minimumDeleteSum;
+}
+  
---
+++ minimumDeleteSum/minimumDeleteSum.test.js
@@ -0,0 +1,11 @@
+import minimumDeleteSum from './minimumDeleteSum';
+
+describe('minimumDeleteSum', () => {
+  test('is a function', () => {
+    expect(minimumDeleteSum).toBeInstanceOf(Function);
+  });
+
+  // test('it returns true', () => {
+  //   expect(addTwoNumbers(5)).toBeTruthy();
+  // });
+});
